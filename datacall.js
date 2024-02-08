4e73cc218b8cc183db203f07dd60a81ae35f6909
CODECZERO <as8857981@gmail.com>
2024-02-09 01:02:40 +0530

    add fetch in the js script

--- datacall.js
+++ datacall.js
@@ -1,4 +1,3 @@
-const req=new XMLHttpRequest();
 const rm=()=>{
 	let rm=document.querySelector("#rm");
      rm.remove();
@@ -7,23 +6,25 @@ const rm=()=>{
 	search.addEventListener("click",()=>{
 			let container=document.querySelector("#cont");
 			let searchin=document.querySelector("#searchf").value;
-    		req.open('GET',`https://restcountries.com/v3.1/name/${searchin}`);   
-    		req.send();
-    		req.addEventListener('load',function(){
-        	let [data]=JSON.parse(this.responseText);
-        	const HtmlData=`<div id="rm" class="card-conta">
-            		<div class="card-main">
-                		<h1>${data.flag}</h1>
-                		<div class="card-content">
-                    		<h3>Country name:${data.name.official}</h3>
-                    		<p>Capital:${data.capital}</p><br>
-                    		<p>Region:${(data.region)}</p><br>
-                    		<p>Population:${data.population}</p><br>
-                    		<p>Borders:${data.borders}</p><br>
-                    		<button id="rm" type="button" class="btncss" onclick="rm()">Remove</button>
-                		</div>
-            		</div>
-        		</div>`;
-    	container.insertAdjacentHTML("afterbegin",HtmlData);
-		});
-	});
+			fetch(`https://restcountries.com/v3.1/name/${searchin}`)
+			.then(resp => resp.json())
+			.then(res=>{let [data]=res;
+				console.dir(data);
+				const HtmlData=`<div id="rm" class="card-conta">
+						<div class="card-main">
+							<h1>${data.flag}</h1>
+							<div class="card-content">
+								<h3>Country name:${data.name.official}</h3>
+								<p>Capital:${data.capital}</p><br>
+								<p>Region:${(data.region)}</p><br>
+								<p>Population:${data.population}</p><br>
+								<p>Borders:${data.borders}</p><br>
+								<button id="rm" type="button" class="btncss" onclick="rm()">Remove</button>
+							</div>
+						</div>
+					</div>`;
+				container.insertAdjacentHTML("afterbegin",HtmlData);
+			})
+			.catch((err)=>{alert(err);})
+        	
+});
